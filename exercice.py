20f1cc48166f538a1cbb863d0b22d7deffd34906
Anneraymond08 <90001135+Anneraymond08@users.noreply.github.com>
2021-10-28 14:17:05 -0400

    done

--- exercice.py
+++ exercice.py
@@ -5,6 +5,7 @@
 import math
 import sys
 from turtle import *
+import re
 
 sys.path.insert(0, 'C:\\Users\\anner\\OneDrive\\Bureau\\intro programmation (inf1007)\\exercices\\2021a-c01-ch6-1-exercices-Anneraymond08')
 from exercice_chap6 import frequence
@@ -33,13 +34,32 @@ def draw_branch(branch_len,pen_size,angle):
         right(angle)
         backward(branch_len)
 
+def valide(sequence):
+    return bool(re.match("^[actg]+$", sequence))
 
+def saisie(type):
+    value = input(f"{type}:")
+    if valide(value):
+        return value
+    print(f"La {type} n'est pas valide.")
+    return saisie(type)
+
+def proportion(chaine, sequence):
+    return chaine.count(sequence) * len(sequence) / len(chaine)
+
+def check_dan():
+    chaine = saisie("chaine")
+    sequence = saisie("sequence")
+    print(f"Il y a {proportion(chaine,sequence) * 100}% de {sequence}.")
 
 if __name__ == '__main__':
     # TODO: Appelez vos fonctions ici
     volume, masse = ellipsoide(2,3,4,10)
-    print(volume)
-    print(masse)
-    print((lambda sentence: sorted(frequence(sentence).items(), key=lambda x: x[1])[-1][0])('Je suis une phrase eee'))
-
-    draw_tree()
+    #print(volume)
+    #print(masse)
+    #print((lambda sentence: sorted(frequence(sentence).items(), key=lambda x: x[1])[-1][0])('Je suis une phrase eee'))
+    #draw_tree()
+    #print(valide('aacg'))
+    #saisie("chaine")
+    #print(proportion("actgaaatcgactgactg", "actg"))
+    check_dan()
